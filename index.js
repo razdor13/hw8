b7eaeb073d8cbb9332059c20df6b436ad6e3237d
Razdor13 <oneamaday@gmail.com>
2023-12-13 17:54:38 +0200

    done

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const elementsImput = prompt('Enter number of list  to separating them by ","');
 
-const userArr = elementsImput.split(",").map(function (elem) {
-    return +elem;
-});
-const isnumber = userArr.some(function (elem) {
-    return isNaN(elem);
-});
-console.log(userArr);
-
 let sum = 0;
 if (elementsImput === null) {
     alert("ok gl");
 } else if (!elementsImput) {
     alert("nothing entered");
-} else if (isnumber) {
-    alert("not all elements are numbers!");
 } else {
-    userArr.forEach(function (elem) {
-        sum += elem;
+    const userArr = elementsImput.split(",").map(function (elem) {
+        return +elem;
     });
-    alert("сума " + sum);
+    const isnumber = userArr.some(function (elem) {
+        return isNaN(elem);
+    });
+    if (isnumber) {
+        alert("not all elements are numbers!");
+    } else {
+        userArr.forEach(function (elem) {
+            sum += elem;
+        });
+        alert("сума " + sum);
+    }
 }
